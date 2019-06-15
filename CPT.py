6a37f09e347da4c7e9bc68943912d14fcda4ae40
nathanielfernandes <51859896+nathanielfernandes@users.noreply.github.com>
2019-06-15 13:18:30 -0400

    Add files via upload

--- CPT.py
+++ CPT.py
@@ -28,6 +28,7 @@ lavaColor = [255, 42, 0]
 
 # This not our code, we got it from (https://stackoverflow.com/questions/14044147/animated-sprite-from-few-images)
 # This subprogram makes it easier to load images to pygame
+# we improved the code for different cases
 def load_image(name, opaque, color):
 
     # if the image is completely opaque
@@ -93,6 +94,7 @@ class falling_rock(pygame.sprite.Sprite):
 
         self.rect.y += self.speed
 
+        # make sure the color stays within the right range
         if (self.color[1] > 200):
             self.flip *= -1
         elif (self.color[1] < 40):
@@ -120,36 +122,25 @@ class player(pygame.sprite.Sprite):
         self.speed1 = 10
         self.speed2 = 10
 
-    # subprogram 
+    # subprogram to keep the player on the screen
     def collision(self):
 
-        if (self.rect.y > screen.get_height() - 80):
+        if (self.rect.y > 920):
             self.speed2 = 0
-            self.rect.y = screen.get_height() - 80
-        if (self.rect.x >= screen.get_width() - 30):
+            self.rect.y = 920
+        if (self.rect.x >= 1470):
             self.speed1 = 0
-            self.rect.x = screen.get_width() - 30
+            self.rect.x = 1470
         elif (self.rect.x <= 5):
             self.speed1 = 0
             self.rect.x = 5
 
-
+    # supprgram that simulates a constant gravity on the player
     def gravity(self):
 
-
         self.speed2 += 2
-        self.rect.y += self.speed2
+        self.rect.y += self.speed
 
-        # Check the walls
-        # if (self.rect.x >= screen.get_width()):
-        #     self.speed1 = self.speed1 * -1
-        #     self.rect.x = screen.get_width()
-        # elif (self.rect.x <= 0):
-        #     self.speed1 = self.speed1 * -1
-        #     self.rect.x = 0
-        # if (self.rect.y > screen.get_height() - 100):
-        #     self.speed2 = 0
-        #     self.rect.y = screen.get_height() - 100
         if (self.rect.y <= 0):
             self.speed2 = self.speed2 * -0.5
             self.rect.y = 5
@@ -159,54 +150,49 @@ class player(pygame.sprite.Sprite):
 pygame.init()
 
 # Set the height and width of the screen
-
 size = (1500, 1000)
 
+# creates the screen
 screen = pygame.display.set_mode(size)
 
-# This is a list of 'sprites.' Each block in the program is
+# This is a list of 'sprites.' Each object in the program is
 # added to this list. The list is managed by a class called 'Group.'
-
 rock_list = pygame.sprite.Group()
 player_list = pygame.sprite.Group()
-lava_list = pygame.sprite.Group()
+
 # This is a list of every sprite.
-# All blocks and the player block as well.
+# All rocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
 
+# creates the floor sprite
 floor = floor_rock(WHITE, screen.get_width(), 100)
-
 floor.rect.x = 0
 floor.rect.y = screen.get_height() + 100
 
+all_sprites_list.add(floor)
+
+
 rock_speed = 5
 
+# subprogram which creates a rock when called
 def createRock(speed):
 
-    #for i in range(numRocks):
-    
     rock = falling_rock( 60, 60, speed)
-
     rock.rect.x = random.randrange(0, size[0])
     rock.rect.y = random.randrange(-1*size[1], 0)
-        
-        
+    
     rock_list.add(rock)
     all_sprites_list.add(rock)
 
-    # lava = floor_lava(RED, 25, 10)
-    # lava.rect.x = -50
-    # lava.rect.y = -50
 
-    # rock_list.add(lava)
-    # all_sprites_list.add(lava)
-
-#player 1
+# creates the player sprite
 player1 = player(R_1, 40, 60, 14)
 player1.rect.x = 750
 player1.rect.y = 10
+
 player_list.add(player1)
 all_sprites_list.add(player1)
+
 
 # Loop until the user clicks the close button.
 done = False
@@ -215,24 +201,23 @@ done = False
 clock = pygame.time.Clock()
 
 score = 0
+
+# this is not our code, we got it from ()
+# variables used for allowing the player to jump
 jump = False
 high = 15
 jumpcount = high
-initial_rock = True
-numRocks = 1
-time = 0
 
+# used to create the first set of rocks
+initial_rock = True
+
+
+# used for changing colors
 colorFlip = 1
 
-
-
-
+# loads the background image 
 background_image = load_image("background2.png", False, WHITE)
 background_image = pygame.transform.scale(background_image, (1500, 1000))
-
-
-#player_image = pygame.image.load("baljeet.png").convert()
-#rock_image.set#
 
 
 # -------- Main Program Loop -----------
@@ -240,17 +225,24 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    #     if (event.type == pygame.KEYDOWN):
+    #if (event.type == pygame.KEYDOWN):
 
+    # gets the user key inputs
     keys = pygame.key.get_pressed()
 
+
+    # player movement
+
+    # lateral movement
     if keys[pygame.K_a]:
         player1.rect.x -= player1.speed
     if keys[pygame.K_d]:
         player1.rect.x += player1.speed
 
+    # this is not our code, we got it from ()
+    # allows the player to jump
     if not (jump):
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_SPACE]:
             jump = True
     else:
         if jumpcount >= -high:
@@ -263,16 +255,14 @@ while not done:
             jump = False
             jumpcount = high
 
-    screen.fill(WHITE)
-
+    # used to make the lava in the background image change color
     pygame.draw.rect(screen, lavaColor, [650, 120, 350, 300])
+
     if (lavaColor[1] > 200):
         colorFlip *= -1
     elif (lavaColor[1] < 1):
         colorFlip *= -1
-
     lavaColor[1] += colorFlip
-
 
     screen.blit(background_image,(0, 0))
 
@@ -280,36 +270,37 @@ while not done:
 
     # Get the current mouse position. This returns the position
     # as a list of two numbers.
-
-
     # Fetch the x and y out of the list,
        # just like we'd fetch letters out of a string.
     # Set the player object to the mouse location
 
+    # creates the first set of rocks when the game begins
     if initial_rock:
         for i in range(15):
             createRock(rock_speed)
             initial_rock = False
 
-    # See if the player block has collided with anything.
+    # See if the player has collided with any rock.
     rock_collisions = pygame.sprite.spritecollide(player1, rock_list, False)
+
+    # See if any rocks have collided with the floor.
     rock_floor = pygame.sprite.spritecollide(floor, rock_list, True)
 
-
-    #Check the list of collisions.
+    # runs for every player-rock collision
     for rock in rock_collisions:
         score += 1
         print(score)
 
-    
+    # runs for every floor-rock collision
     for rock in rock_floor:
         createRock(rock_speed)
         rock_speed += 0.02
 
-
+    # calls the update function of each rock
     for rock in rock_list:
         rock.update()
 
+    # calls the gravity and collision for player1
     player1.gravity()
     player1.collision()
 
